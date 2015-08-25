2cdeb4fd3b3022a1c82b6a5a5e5d1df38952c5a9
Rashmi Manandhar <rashmi.manandhar7@gmail.com>
2015-08-25 14:54:00 +0545

    index.php

--- index.php
+++ index.php
@@ -11,9 +11,15 @@ class InMemoryDataSet implements IDataSet {
 		$this->_data = $data;
 	}
 
+	public function getName(){
+		return $this->_data[0];
+	}
+
 	public function getRowCount() {
 		return count($this->_data);
 	}
+
+
 
 	public function getValueAt($row, $column) {
 		if ($row >= $this->getRowCount()) {
@@ -30,26 +36,72 @@ function CSVToDataSet($file) {
 	return new InMemoryDataSet(array_map('str_getcsv', file($file)));
 }
 
-class DataQuery {
-	private $_dataSet;
+// class DataQuery {
+// 	private $_dataSet;
 
-	public function __construct(IDataSet $dataSet) {
-		$this->_dataSet = $dataSet;
+// 	public function __construct(IDataSet $dataSet) {
+// 		$this->_dataSet = $dataSet;
 
+// 	}
+
+// 	public function getRowsWithDuplicates($columnIndex) {
+// 		$values = array();
+// 		for ($i = 0; $i < $this->_dataSet->getRowCount(); ++$i) {
+// 			$values[$this->_dataSet->getValueAt($i, $columnIndex)][] = $i;
+// 		}
+
+// 		return array_filter($values, function($row) { return count($row) > 1; });
+// 	}
+// }
+
+/**
+*  
+*/
+class Merge 
+{
+	private $obj_a;
+	private $obj_b;
+	
+	public function __construct($obj_a, $obj_b)
+	{
+		$this->obj_a = $obj_a;
+		$this->obj_b = $obj_b;
 	}
 
-	public function getRowsWithDuplicates($columnIndex) {
-        $values = array();
-        for ($i = 0; $i < $this->_dataSet->getRowCount(); ++$i) {
-            $values[$this->_dataSet->getValueAt($i, $columnIndex)][] = $i;
-        }
-
-        return array_filter($values, function($row) { return count($row) > 1; });
+	public function getName()
+	{
+		return $this->obj_a->getName();
+		//var_dump($output);die('here');
 	}
+
+	public function merge($a, $b)
+	{
+		var_dump($a);
+		$keys = array_keys($a);
+		foreach ($keys as $key) {
+			$final[] = array_merge($a[$key], $b[$key]);
+			# code...
+		}
+
+		// // echo "<pre>";
+		// // var_dump($this->obj_a);
+		// // var_dump($this->obj_b);
+		// $merged = array_merge($a, $b);
+
+		 echo "<pre>";
+		 var_dump($final, 'have fun');die;
+	}
+
+
+
 }
 
-$dataSet = CSVToDataSet("awards.csv");
-$query = new DataQuery($dataSet);
-$dupes = $query->getRowsWithDuplicates(0);
-echo"<pre>";
-var_dump($query);
+$awardsCSV = (array)CSVToDataSet("awards.csv");
+$contractsCSV = (array)CSVToDataSet("contracts.csv");
+
+// $awards = new DataQuery($awardsCSV);
+// $contracts = new DataQuery($contractsCSV);
+$output = new Merge($awardsCSV, $contractsCSV);
+$dupes = $output->merge($awardsCSV, $contractsCSV);
+
+var_dump($dupes);
